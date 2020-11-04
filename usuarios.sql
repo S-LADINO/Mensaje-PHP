44c1dc93ed5d9b2644aaddede6cc6d1940631bc1
S-LADINO <73620784+S-LADINO@users.noreply.github.com>
2020-11-03 21:28:05 -0500

    Update usuarios.sql
    actualizacion

--- usuarios.sql
+++ usuarios.sql
@@ -5,17 +5,13 @@
 -- Servidor: 127.0.0.1
 -- Tiempo de generación: 04-11-2020 a las 02:49:31
 -- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- 
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `test`
