56322bf23b36ad5fd9a8bee7fedceed79a2f4eef
S-LADINO <73620784+S-LADINO@users.noreply.github.com>
2020-11-04 21:15:17 -0500

    Se actualiza registro

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
-1 <? Php					
+1 <?  Php					
 2
 (yo					
-3    si					
+3     si					
 4					
 6					
 7					
 9					
-"sset ($ dia)) {echo" "si existe"					
-"echo" "no existe"					
-"=" "miércoles" ";"					
-"la va iable \ n" ";"					
-"la va riable \ n" ";"					
+"sset ($ dia)) {echo"  "si existe"					
+"echo"  "no existe"					
+"="  "miércoles"  ";"					
+"la va iable \ n"  ";"					
+"la va riable \ n"  ";"					
 }mi					
 }					
 $ dia					
 $ dia ;					
-10  eco					
+10   eco					
 11					
-12  ?>					
-13   <! DOCTYPE htm					
+12   ?>					
+13    <! DOCTYPE htm					
 15 <cabeza >					
-17      < t  1tle > Aplicación Mensajes </ t 1tle					
-18     </ heac  >					
-19   < .: Iody >					
-"20       < , > hola, y bienvenid @ sa m"					
-21     </ ood  >					
+17       <  t   1tle  > Aplicación Mensajes </  t 1tle					
+18      </  heac   >					
+19    < .: Iody >					
+"20        < , > hola, y bienvenid @ sa m"					
+21      </  ood   >					
 "22 ht l" "l 1 > "					
 l >					
 >					
