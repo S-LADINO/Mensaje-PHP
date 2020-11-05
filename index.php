8a3e6cb3b42bd480531da05d283d68ddbefa1900
S-LADINO <73620784+S-LADINO@users.noreply.github.com>
2020-11-04 19:19:33 -0500

    indice de aplicacion web

---
+++ index.php
@@ -0,0 +1,33 @@
+1 <?php					
+2					
+(i					
+3   if					
+4					
+6					
+7					
+9					
+"sset($dia)) { echo ""si existe"					
+"echo ""no existe"					
+"= ""miércoles"";"					
+"la va iable\n"";"					
+"la va riable\n"";"					
+}e					
+}					
+$dia					
+$dia;					
+10 echo					
+11					
+12 ?>					
+13  <!DOCTYPE htm					
+15    <head>					
+17     <t 1tle>Mensajes App</t 1tle					
+18    </heac >					
+19  <.:iody>					
+"20      <,>hola, y bienvenid@s a m"					
+21    </ood >					
+"22  h t l""l 1>"					
+l>					
+>					
+"ensajes app, cálido.</"					
+>					
+</				
