2231d2dcd70f31043b93f4ddd8f7eba71b727918
S-LADINO <73620784+S-LADINO@users.noreply.github.com>
2020-11-05 19:01:54 -0500

    actualizacion de cogido, se agrega color a la interfaz web

--- index.php
+++ index.php
@@ -1,33 +1,7 @@
-1 <?  Php					
-2
-(yo					
-3     si					
-4					
-6					
-7					
-9					
-"sset ($ dia)) {echo"  "si existe"					
-"echo"  "no existe"					
-"="  "miércoles"  ";"					
-"la va iable \ n"  ";"					
-"la va riable \ n"  ";"					
-}mi					
-}					
-$ dia					
-$ dia ;					
-10   eco					
-11					
-12   ?>					
-13    <! DOCTYPE htm					
-15 <cabeza >					
-17       <  t   1tle  > Aplicación Mensajes </  t 1tle					
-18      </  heac   >					
-19    < .: Iody >					
-"20        < , > hola, y bienvenid @ sa m"					
-21      </  ood   >					
-"22 ht l" "l 1 > "					
-l >					
->					
-"aplicación ensajes, cálido. </ "					
->					
-</				 
+<?php
+	if (isset($_session['id_usuario'])) {
+		header("Location: panel.php");
+	}else {
+		header("Location: login.php");
+	}
+?>	 
